612b34dcd7438b846572b9a4264e2920cba2f437
Alexandre This <alexandre.this@gmail.com>
2014-11-23 13:27:16 +0100

    updated test.m

--- Matlab/AMT/AMT/getFext.m
+++ Matlab/AMT/AMT/getFext.m
@@ -3,11 +3,23 @@ noisyU = imread(imName);
 noisyU = downsample2D(noisyU,downscaleFactor);
 f = double(noisyU);
 
+SobelX = [-1,0,1;-2,0,2;-1,0,1];
+SobelY = [-1,-2,-1;0,0,0;1,2,1];
+
+Gx = conv2(f,SobelX);
+Gx = Gx(2:end-1,2:end-1);
+Gy = conv2(f,SobelY);
+Gy = Gy(2:end-1,2:end-1);
+
+Eext = sqrt(Gx.*Gx+Gy.*Gy);
+
+imshow(Eext,[min(min(Eext)),max(max(Eext))]);
+pause();
 
 
-sizeFFT = size(f)+size(Fx)-1;
+sizeFFT = size(Eext)+size(Fx)-1;
 
-fftN = fft2(f,sizeFFT(1),sizeFFT(2));
+fftN = fft2(Eext,sizeFFT(1),sizeFFT(2));
 fftFX = fft2(Fx,sizeFFT(1),sizeFFT(2));
 fftFY = fft2(Fy,sizeFFT(1),sizeFFT(2));
 
@@ -19,36 +31,16 @@ rmv = (size(Fx) - 1)/2;
 vectorFieldX = vectorFieldX(rmv(1)+1:end-rmv(1), rmv(2)+1:end-rmv(2), :);
 vectorFieldY = vectorFieldY(rmv(1)+1:end-rmv(1), rmv(2)+1:end-rmv(2), :);
 
-s2 = size(f);
+s2 = size(Eext);
 
 Fext = zeros(s2(1),s2(2),2);
+
 Fext(:,:,1) = vectorFieldX;
 Fext(:,:,2) = vectorFieldY;
 
-
-[Gx,Gy] = gradient(f);
-
-
-
-I = sqrt(Gx.*Gx+Gy.*Gy);
-I = im2bw(I,graythresh(I));
-
-for i = 1:s2(1)
-    for j = 1:s2(2)
-        if(I(i,j) )
-            Fext(i,j,1) = Gx(i,j);
-            Fext(i,j,2) = Gy(i,j);
-        end
-    end
-end
-
-Fmag = sqrt(sum(Fext.*Fext,3))+eps;
+Fsq = Fext.*Fext;
+Fmag = sqrt(Fsq(:,:,1)+Fsq(:,:,2)) + eps;
 Fext(:,:,1) = Fext(:,:,1)./Fmag;
 Fext(:,:,2) = Fext(:,:,2)./Fmag;
-
-
-figure()
-[x_im,y_im] = meshgrid(1:1:s2(2),1:1:s2(1));
-quiver(x_im,y_im,Fext(:,:,1),Fext(:,:,2));
 
 end
--- Matlab/AMT/AMT/test.m
+++ Matlab/AMT/AMT/test.m
@@ -4,29 +4,30 @@ clc;
 [Fx,Fy] = getKernel(32,1);
 
 alpha = .5;
-beta = 0.0;
+beta = 0.1;
 tau = 0.5;
 RES = 0.5;
-R =20
-vt = AC_initial(RES, 'circle', [32 32 R]);   % seeds
+R =100
+vt = AC_initial(RES, 'circle', [70 70 R]);   % seeds
 
-for i = [1]
-    [Fext,resizedIm] = getFext(Fx,Fy,'im_Unoisy.bmp',i);
-    s = size(Fext);
-    [x_im,y_im] = meshgrid(1:s(2),1:s(1));
+[Fext,resizedIm] = getFext(Fx,Fy,'im_lung.png',4);
+s = size(Fext);
+[x_im,y_im] = meshgrid(1:s(2),1:s(1));
 
-    for i=1:100, % moving these seeds
-        vt = AC_deform(vt,alpha,beta,tau,Fext,5);
-        vt = AC_remesh(vt,1);
-        imshow(resizedIm);
-        hold on;
-        plot(vt(:,1),vt(:,2),'*r');
-        quiver(x_im,y_im,Fext(:,:,1),Fext(:,:,2));
-        hold off
-        pause(0.1);
-    end
-    pause();
-    vt = vt.*2;
+I = imread('im_lung.png');
+I = double(I);
+I = downsample2d(I,4);
+I = I-(min(min(I)));
+I = I./(max(max(I)));
+
+for i=1:100, % moving these seeds
+    vt  = AC_deform(vt,alpha,beta,tau,Fext,5);
+    vt = AC_remesh(vt,1);
+    
+    AC_QUIVER(Fext,I);
+    hold on;
+    plot(vt(:,1),vt(:,2),'.r');
+    hold off
+    pause(0.1);
 end
-
 
