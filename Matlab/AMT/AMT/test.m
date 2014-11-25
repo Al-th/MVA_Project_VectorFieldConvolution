3222d96be08a9d221cbef47ecec6b0fb29a7156c
Alexandre This <alexandre.this@gmail.com>
2014-11-25 22:26:25 +0100

    added image folder

--- Matlab/AMT/AMT/test.m
+++ Matlab/AMT/AMT/test.m
@@ -5,37 +5,45 @@ alpha = .5;
 beta = 0.1;
 tau = 0.5;
 RES = 0.5;
-R = 100;
+C = zeros(2);
+C(1) = 80;
+C(2) = 80;
+R = 70;
 imName = 'im_Unoisy.bmp';
 downsampleFactor = 1;
-edgeMapType = 'sobelMagnitude';
+edgeMapType = 'cannyWeighted';
 
 %%
 
 I = imread(imName);
 I = downsample2d(I,downsampleFactor);
 f = double(I);
-I = downsample2d(I,downsampleFactor);
 
-EdgeMap = getEdgeMap(f,'cannyWeighted');
+edgeMap = getEdgeMap(f,edgeMapType);
 
-imshow(EdgeMap,[min(min(EdgeMap)),max(max(EdgeMap))]);
+imshow(edgeMap,[min(min(edgeMap)),max(max(edgeMap))]);
 pause();
 
+Fext = getFext(Fx,Fy,edgeMap);
 
-[Fext] = getFext(Fx,Fy,EdgeMap);
+f = f-(min(min(f)));
+f = f./(max(max(f)));
 
+if 1
+    print('click in window for center and radius of circle')
+    [x,y] = ginput(2);
+    C(1) = x(1);
+    C(2) = y(1);
 
+    R = sqrt((x(2)-x(1))^2+(y(1)-y(2))^2)
+end
 
-I = I-(min(min(I)));
-I = I./(max(max(I)));
-
-vt = AC_initial(RES, 'circle', [70 70 R]);   % seeds
+vt = AC_initial(RES, 'circle', [C(1) C(2) R]);   % seeds
 for i=1:100, % moving these seeds
     vt  = AC_deform(vt,alpha,beta,tau,Fext,10);
     vt = AC_remesh(vt,1);
     
-    AC_quiver(Fext,I);
+    imshow(edgeMap,[min(min(edgeMap)) max(max(edgeMap))]);
     hold on;
     plot(vt(:,1),vt(:,2),'.r');
     hold off
